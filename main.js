ae911d5f2cba646912f4d4a7aab5c0065e2cceb0
hadihammurabi <hadihammurabi@gmail.com>
2023-07-26 11:42:03 +0700

    use better error message

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 function onSubmit(e) {
-    e.preventDefault();
-    alert('upsss...!!! akun salah.');
+  e.preventDefault();
+  alert('upsss...!!! akun salah, coba lagi!');
 }
 
 document.querySelector('form').addEventListener('submit', onSubmit);
